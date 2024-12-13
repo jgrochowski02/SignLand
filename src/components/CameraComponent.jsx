bc9d0d94a6280f286546a59ebd8abdd5485beef0
jgrochowski02 <jgrochowski02@wp.pl>
2024-12-13 22:05:06 +0100

    add: taking photos

--- src/components/CameraComponent.jsx
+++ src/components/CameraComponent.jsx
@@ -1,20 +1,23 @@
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState } from 'react';
-import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { useState, useRef } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View, Image, Alert } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import PhotoPreviewSection from './PhotoPreviewSection';
 
-export default function App() {
+export default function Camera() {
   const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
+  const [photo, setPhoto] = useState(null);
+  const cameraRef = useRef(null);
 
   if (!permission) {
-    
     return <View />;
   }
 
   if (!permission.granted) {
     return (
       <View style={styles.container}>
-        <Text style={styles.message}>We need your permission to show the camera</Text>
+        <Text style={styles.message}>Potrzebujesz uprawnień do otworzenia aparatu.</Text>
         <Button onPress={requestPermission} title="grant permission" />
       </View>
     );
@@ -24,18 +27,45 @@ export default function App() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
+  const handleTakePhoto = async() =>{
+    if(cameraRef.current){
+      const options = {
+        quality: 1,
+        base64: true,
+       exif: false,
+        };
+        const takedPhoto = await cameraRef.current.takePictureAsync(options);
+
+        setPhoto(takedPhoto);
+      }
+    };
+  
+  const handleRetakePhoto = () => {
+    setPhoto(null);
+  };
+
+  if(photo){
+    return(
+      <PhotoPreviewSection photo={photo} handleRetakePhoto={handleRetakePhoto} />
+    )
+  }
+
   return (
     <View style={styles.container}>
-      <CameraView style={styles.camera} facing={facing}>
+      <CameraView style={styles.camera} facing={facing} ref={cameraRef}>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
-            <Text style={styles.text}>Flip Camera</Text>
+            <AntDesign name="retweet" size={44} color="black" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleTakePhoto}>
+            <AntDesign name="camera" size={44} color="black" />
           </TouchableOpacity>
         </View>
       </CameraView>
     </View>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -59,6 +89,9 @@ const styles = StyleSheet.create({
     flex: 1,
     alignSelf: 'flex-end',
     alignItems: 'center',
+    marginHorizontal: 10,
+    backgroundColor: 'grey',
+    borderRadius: 10
   },
   text: {
     fontSize: 24,
