cf54aff74f87271bab76aa15b6353b60fbf0fdbd
jgrochowski02 <jgrochowski02@wp.pl>
2024-12-13 17:19:41 +0100

    fix: camera

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,20 +1,17 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import Header from '../components/Header';
 import BottomMenu from '../components/BottomMenu';
+import CameraComponent from '../components/CameraComponent';
 import styles from '../utils/styles';
 
 const HomeScreen = ({ navigation }) => {
-    const [photoUri, setPhotoUri] = useState(null);
-
-    // Funkcja przejścia do ekranu wyniku z przesłanym zdjęciem
-    const goToProgress = (photoName) => {
-        navigation.navigate('ResultScreen', { photoName });
-    };
-
+  
     return (
         <View style={styles.containerNoCenter}>
             <Header title="ZDJĘCIE" />
+            
+                <CameraComponent />
             
             <BottomMenu navigation={navigation} />
         </View>
